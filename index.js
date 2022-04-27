261a6b016caeb8a5f5b5c0b48cb18336f27b6160
vli417 <iml3676@nyu.edu>
2022-04-26 22:22:07 -0400

    first push

---
+++ index.js
@@ -0,0 +1,8 @@
+var buttons = document.querySelectorAll('.television__channel a');
+
+for(var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function(e) {
+    document.querySelector('.television__screen iframe').src = this.href;
+    e.preventDefault();
+  });
+}
